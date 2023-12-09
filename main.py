6dd5a2501b3bb1f2b11b4b3d7cf55f067fa8e004
ntgowot <jb3499711@gmail.com>
2023-12-09 14:31:14 +0000

    migrate to match statements

--- main.py
+++ main.py
@@ -7,14 +7,15 @@ print("type help for help, or exit to exit")
 while True:
     cmd = input("→ ")
     split_cmd = cmd.split()
-    if split_cmd[0] == "exit":
-        break
-    elif split_cmd[0] == "help":
-        help.help()
-    elif split_cmd[0] == "echo":
-        try:
-            echo.echo(split_cmd[1])
-        except IndexError:
-            print("not enough arguments →", split_cmd) # prints split_cmd for now, will fix later
-    else:
-        print("invalid command →", split_cmd[0])
+    match split_cmd[0]:
+        case "exit":
+            break
+        case "help":
+            help.help()
+        case "echo":
+            try:
+                echo.echo(split_cmd[1])
+            except IndexError:
+                print("not enough arguments →", split_cmd) # prints split_cmd for now, will fix later
+        case _:
+            print("invalid command →", split_cmd[0])
